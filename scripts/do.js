8d8752716668d8862083b15358c536e991390d7e
Sarven Capadisli <info@csarven.ca>
2016-01-14 16:22:29 +0100

    Add method to generate a hash of string CRC32

--- scripts/do.js
+++ scripts/do.js
@@ -2075,6 +2075,17 @@ LIMIT 1";
             }, '*[class*="highlight-"]');
         },
 
+        hashCode: function(s){
+            var hash = 0;
+            if (s.length == 0) return hash;
+            for (i = 0; i < s.length; i++) {
+                char = s.charCodeAt(i);
+                hash = ((hash<<5)-hash)+char;
+                hash = hash & hash; // Convert to 32bit integer
+            }
+            return hash;
+        },
+
         generateAttributeId: function(prefix, string) {
             prefix = prefix || '';
 
