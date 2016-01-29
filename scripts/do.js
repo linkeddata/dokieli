06e9057de41312055d21c71b09d8fceb225ce998
Sarven Capadisli <info@csarven.ca>
2016-01-29 17:59:22 -0500

    Minor

--- scripts/do.js
+++ scripts/do.js
@@ -884,16 +884,15 @@ var DO = {
             $(this).prop('disabled', 'disabled');
             $('body').append('<aside id="user-identity-input" class="do on"><button class="close">❌</button><h2>Enter WebID to sign in with</h2><label>HTTP(S) IRI</label><input id="webid" type="text" placeholder="http://csarven.ca/#i" value="" name="webid"/> <button class="signin">Sign in</button></aside>');
 
-            var butttonSignIn = $('#user-identity-input button.signin');
-
-            butttonSignIn.prop('disabled', 'disabled');
+            var buttonSignIn = $('#user-identity-input button.signin');
+            buttonSignIn.prop('disabled', 'disabled');
 
             $('#user-identity-input').on('click', 'button.close', function(e) {
                 $('#document-menu > header .signin-user').removeAttr('disabled');
             });
 
             $('#user-identity-input').on('click', 'button.signin', DO.U.submitSignIn);
-            $('#user-identity-input').on('keyup', 'input#webid', butttonSignIn, DO.U.enableDisableButton);
+            $('#user-identity-input').on('keyup', 'input#webid', buttonSignIn, DO.U.enableDisableButton);
 
             $('#user-identity-input input#webid').focus();
         },
