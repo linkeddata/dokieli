0f07a20e11b58702811dc5a6b5f0c341edb0106b
Sarven Capadisli <info@csarven.ca>
2016-03-02 14:10:10 -0500

    Remove jQuery from exportAsHTML. Issue #47

--- scripts/do.js
+++ scripts/do.js
@@ -1807,7 +1807,7 @@ var DO = {
             //XXX: Encodes strings as UTF-8. Consider storing bytes instead?
             var blob = new Blob([data], {type:'text/html;charset=utf-8'});
             var pattern = /[^\w]+/ig;
-            var title = $('h1').text().toLowerCase().replace(pattern, '-') || "index";
+            var title = document.querySelector('h1').textContent.toLowerCase().replace(pattern, '-') || "index";
             var timestamp = DO.U.getDateTimeISO().replace(pattern, '') || "now";
 
             var fileName = title + '.' + timestamp + '.html';
