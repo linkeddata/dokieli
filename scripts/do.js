a872aa54c9d934eff572176146a2310db91823f9
Sarven Capadisli <info@csarven.ca>
2016-06-03 09:55:52 -0400

    Add skos:prefLabel to vocab context

--- scripts/do.js
+++ scripts/do.js
@@ -108,6 +108,8 @@ var DO = {
             "dctermsissued": "http://purl.org/dc/terms/issued",
             "dctermscreated": "http://purl.org/dc/terms/created",
 
+            "skosprefLabel": { "@id": "http://www.w3.org/2004/02/skos/core#prefLabel", "@type": "@id", "@array": true },
+
             "biboauthorList": { "@id": "http://purl.org/ontology/bibo/authorList", "@type": "@id" },
 
             "storage": { "@id": "http://www.w3.org/ns/pim/space#storage", "@type": "@id", "@array": true },
