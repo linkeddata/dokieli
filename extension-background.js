aa15497958c0c203cf7e6969ea843458dce51a76
Sarven Capadisli <info@csarven.ca>
2017-07-30 20:33:49 +0200

    Move extension insertCSS to background

--- extension-background.js
+++ extension-background.js
@@ -26,6 +26,8 @@ function injectResources(tabId, files) {
         resolve();
       }
     });
+
+    browser.tabs.insertCSS({"code": "@font-face{font-family:'FontAwesome' ;src:url('" + browser.extension.getURL('/media/fonts/fontawesome-webfont.eot?v=4.7.0') + "');src:url('" + browser.extension.getURL('/media/fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') + "') format('embedded-opentype'),url('" + browser.extension.getURL('/media/fonts/fontawesome-webfont.woff2?v=4.7.0') + "') format('woff2'),url('" + browser.extension.getURL('/media/fonts/fontawesome-webfont.woff?v=4.7.0') + "') format('woff'),url('" + browser.extension.getURL('/media/fonts/fontawesome-webfont.ttf?v=4.7.0') + "') format('truetype'),url('" + browser.extension.getURL('/media/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') + "') format('svg'); }"});
   })));
 }
 
