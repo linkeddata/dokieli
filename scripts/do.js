d46a5e9266b2dc9b31c8ff09905425e4226f3ab4
Sarven Capadisli <info@csarven.ca>
2017-08-30 15:24:21 +0200

    Include license in svg from parameter

--- scripts/do.js
+++ scripts/do.js
@@ -890,6 +890,13 @@ var DO = {
       svg.attr('xml:lang', 'en');
       svg.attr('prefix', 'rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns# rdfs: http://www.w3.org/2000/01/rdf-schema# xsd: http://www.w3.org/2001/XMLSchema# schema: http://schema.org/');
 
+      if('license' in options) {
+        svg.append('metdata')
+          .append('tspan')
+            .attr('rel', 'schema:license')
+            .attr('resource', options.license)
+      }
+
       if ('title' in options) {
         svg.append('title')
           .attr('property', 'schema:name');
