ab9523a8f70c58399f095081d81d2a55112a6786
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-09-14 15:05:09 -0400

    Create a webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: [
+    './src/dokieli.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/scripts/'),
+    filename: 'do.js',
+    library: 'DO',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: {
+  },
+  devtool: 'source-map'
+}
