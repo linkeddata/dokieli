bb14cc2a63e48a8e21afcb7ed2a9544faf295d39
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-10-03 16:46:39 -0400

    Move parseLinkHeader to src/fetcher.js

--- src/fetcher.js
+++ src/fetcher.js
@@ -17,6 +17,7 @@ module.exports = {
   getResourceHead,
   getResourceGraph,
   getResourceOptions,
+  parseLinkHeader,
   patchResource,
   postResource,
   putResource,
@@ -300,6 +301,36 @@ function getResourceOptions (url, options = {}) {
 
       return { headers: response.headers }  // Not currently used anywhere
     })
+}
+
+function parseLinkHeader (link) {
+  if (!link) {
+    return {}
+  }
+  var linkexp = /<[^>]*>\s*(\s*\s*[^()<>@,;:"/[\]?={} \t]+=(([^()<>@,;:"/[\]?={} \t]+)|("[^"]*")))*(,|$)/g
+  var paramexp = /[^()<>@,;:"/[\]?={} \t]+=(([^()<>@,;:"/[\]?={} \t]+)|("[^"]*"))/g
+  var matches = link.match(linkexp)
+  var rels = {}
+  for (var i = 0; i < matches.length; i++) {
+    var split = matches[i].split('>')
+    var href = split[0].substring(1)
+    var ps = split[1]
+    var s = ps.match(paramexp)
+    for (var j = 0; j < s.length; j++) {
+      var p = s[j]
+      var paramsplit = p.split('=')
+      // var name = paramsplit[0]
+      var rel = paramsplit[1].replace(/["']/g, '')
+      if (!rels[rel]) {
+        rels[rel] = []
+      }
+      rels[rel].push(href)
+      if (rels[rel].length > 1) {
+        rels[rel].sort()
+      }
+    }
+  }
+  return rels
 }
 
 function patchResource (url, deleteBGP, insertBGP, options = {}) {
