db1e42018a7c48a5d755e10b4d3b35c5562e3a36
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-10-03 16:47:28 -0400

    Move getDocument and related to src/doc.js

---
+++ src/doc.js
@@ -0,0 +1,140 @@
+'use strict'
+
+const Config = require('./config')
+
+module.exports = {
+  domToString,
+  dumpNode,
+  getDoctype,
+  getDocument
+}
+
+function domToString (node, options = {}) {
+  var selfClosing = []
+
+  if ('selfClosing' in options) {
+    options.selfClosing.split(' ').forEach(function (n) {
+      selfClosing[n] = true
+    })
+  }
+
+  var skipAttributes = []
+
+  if ('skipAttributes' in options) {
+    options.skipAttributes.split(' ').forEach(function (n) {
+      skipAttributes[n] = true
+    })
+  }
+
+  var noEsc = [ false ]
+
+  return dumpNode(node, options, skipAttributes, selfClosing, noEsc)
+}
+
+function dumpNode (node, options, skipAttributes, selfClosing, noEsc) {
+  var out = ''
+
+  if (typeof node.nodeType === 'undefined') return out
+
+  if (node.nodeType === 1) {
+    if (node.hasAttribute('class') && 'classWithChildText' in options &&
+        node.matches(options.classWithChildText.class)) {
+      out += node.querySelector(options.classWithChildText.element).textContent
+    } else if (!('skipNodeWithClass' in options && node.matches('.' + options.skipNodeWithClass))) {
+      var ename = node.nodeName.toLowerCase()
+      out += '<' + ename
+
+      var attrList = []
+
+      for (let i = node.attributes.length - 1; i >= 0; i--) {
+        var atn = node.attributes[i]
+
+        if (skipAttributes[atn.name]) continue
+
+        if (/^\d+$/.test(atn.name)) continue
+
+        if (atn.name === 'class' && 'replaceClassItemWith' in options) {
+          atn.value.split(' ').forEach(function (aValue) {
+            if (options.replaceClassItemWith.source.split(' ').indexOf(aValue) > -1) {
+              var re = new RegExp(aValue, 'g')
+              atn.value = atn.value.replace(re, options.replaceClassItemWith.target).trim()
+            }
+          })
+        }
+
+        if (!(atn.name === 'class' && 'skipClassWithValue' in options &&
+            options.skipClassWithValue === atn.value)) {
+          attrList.push(
+            atn.name + '="' +
+            atn.value
+              .replace(/&/g, '&amp;')
+              .replace(/</g, '&lt;')
+              .replace(/>/g, '&gt;')
+              .replace(/"/g, '&quot;') +
+            '"')
+        }
+      }
+
+      if (attrList.length > 0) {
+        if ('sortAttributes' in options && options.sortAttributes) {
+          attrList.sort(function (a, b) {
+            return a.toLowerCase().localeCompare(b.toLowerCase())
+          })
+        }
+        out += ' ' + attrList.join(' ')
+      }
+
+      if (selfClosing[ename]) {
+        out += ' />'
+      } else {
+        out += '>'
+        out += (ename === 'html') ? '\n  ' : ''
+        noEsc.push(ename === 'style' || ename === 'script')
+        for (var i = 0; i < node.childNodes.length; i++) {
+          out += dumpNode(node.childNodes[i])
+        }
+        noEsc.pop()
+        out += (ename === 'body') ? '</' + ename + '>' + '\n' : '</' + ename + '>'
+      }
+    }
+  } else if (node.nodeType === 8) {
+    // FIXME: If comments are not tabbed in source, a new line is not prepended
+    out += '<!--' + node.nodeValue + '-->'
+  } else if (node.nodeType === 3 || node.nodeType === 4) {
+    // XXX: Remove new lines which were added after DOM ready
+    let nl = node.nodeValue.replace(/\n+$/, '')
+    out += noEsc[noEsc.length - 1]
+      ? nl
+      : nl.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  } else {
+    console.log('Warning; Cannot handle serialising nodes of type: ' + node.nodeType)
+  }
+
+  return out
+}
+
+function getDoctype () {
+  /* Get DOCTYPE from http://stackoverflow.com/a/10162353 */
+  var node = document.doctype
+  var doctype = ''
+
+  if (node !== null) {
+    doctype = '<!DOCTYPE ' +
+      node.name +
+      (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') +
+      (!node.publicId && node.systemId ? ' SYSTEM' : '') +
+      (node.systemId ? ' "' + node.systemId + '"' : '') +
+      '>'
+  }
+  return doctype
+}
+
+function getDocument (cn, options) {
+  let node = cn || document.documentElement.cloneNode(true)
+  options = options || Config.DOMNormalisation
+
+  let doctype = getDoctype()
+  let s = (doctype.length > 0) ? doctype + '\n' : ''
+  s += domToString(node, options)
+  return s
+}
