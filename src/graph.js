18ebb0408fd9a374a19a2eb17727a6efa12a0e36
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-10-03 15:27:14 -0400

    Move serializeData and serializeGraph to src/graph.js

--- src/graph.js
+++ src/graph.js
@@ -2,8 +2,12 @@
 
 global.SimpleRDF = (typeof ld !== 'undefined') ? ld.SimpleRDF : undefined
 
+const graph = require('./graph')
+
 module.exports = {
-  getGraphFromData
+  getGraphFromData,
+  serializeData,
+  serializeGraph
 }
 
 function getGraphFromData (data, options = {}) {
@@ -15,4 +19,55 @@ function getGraphFromData (data, options = {}) {
   }
 
   return SimpleRDF.parse(data, options['contentType'], options['subjectURI'])
+}
+
+/**
+ * @param data
+ * @param fromContentType
+ * @param toContentType
+ * @param options
+ *
+ * @returns {Promise}
+ */
+function serializeData (data, fromContentType, toContentType, options) {
+  if (fromContentType === toContentType) {
+    return Promise.resolve(data)
+  }
+
+  options.contentType = fromContentType
+
+  return graph.getGraphFromData(data, options)
+    .then(g => {
+      options.contentType = toContentType
+
+      return graph.serializeGraph(g, options)
+    })
+    .then(data => {
+      switch (toContentType) {
+        case 'application/ld+json':
+          var parsed = JSON.parse(data)
+
+          parsed[0]['@context'] = [
+            'http://www.w3.org/ns/anno.jsonld',
+            {'as': 'https://www.w3.org/ns/activitystreams'}
+          ]
+
+          parsed[0]['@id'] = (parsed[0]['@id'].slice(0, 2) === '_:')
+            ? ''
+            : parsed[0]['@id']
+
+          return JSON.stringify(parsed) + '\n'
+
+        default:
+          return data
+      }
+    })
+}
+
+function serializeGraph (g, options = {}) {
+  if (!('contentType' in options)) {
+    options['contentType'] = 'text/turtle'
+  }
+
+  return ld.store.serializers[options.contentType].serialize(g._graph)
 }
