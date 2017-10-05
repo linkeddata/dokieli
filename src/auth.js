8ae685b6433809d580159f91bd97b51f8333c70d
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-10-05 16:47:36 -0400

    Move enableDisableButton to src/auth.js

--- src/auth.js
+++ src/auth.js
@@ -3,14 +3,41 @@
 const Config = require('./config')
 
 module.exports = {
+  enableDisableButton,
   getUserHTML
+}
+
+// TODO: Generalize this further so that it is not only for submitSignIn
+function enableDisableButton (e, button) {
+  var delay = (e.type === 'cut' || e.type === 'paste') ? 250 : 0
+  var input
+
+  window.setTimeout(function () {
+    input = e.target.value
+    if (input.length > 10 && input.match(/^https?:\/\//g)) {
+      if (typeof e.which !== 'undefined' && e.which === 13) {
+        if (!button.getAttribute('disabled')) {
+          button.setAttribute('disabled', 'disabled')
+          e.preventDefault()
+          e.stopPropagation()
+          DO.U.submitSignIn()
+        }
+      } else {
+        button.removeAttribute('disabled')
+      }
+    } else {
+      if (!button.getAttribute('disabled')) {
+        button.setAttribute('disabled', 'disabled')
+      }
+    }
+  }, delay)
 }
 
 function getUserHTML () {
   let userName = Config.SecretAgentNames[Math.floor(Math.random() * Config.SecretAgentNames.length)]
 
   if (Config.User.Name) {
-        // XXX: We have the IRI already
+    // XXX: We have the IRI already
     userName = '<span about="' + Config.User.IRI + '" property="schema:name">' +
       Config.User.Name + '</span>'
   }
