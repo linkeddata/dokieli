8a2dd9e965abcbf8945810a89a39e6cd74dcc4b0
Sarven Capadisli <info@csarven.ca>
2017-10-27 12:03:07 -0600

    Allow expanded and simple version for createRDFaHTML

--- src/dokieli.js
+++ src/dokieli.js
@@ -4144,46 +4144,56 @@ WHERE {\n\
       return license;
     },
 
-    createRDFaHTML: function(r) {
-      var s = '', property = '', rel = '', resource = '', href = '', content = '', langDatatype = '', typeOf = '';
-      var idValue = DO.U.generateAttributeId();
-      var id = ' id="' + idValue + '"';
+    createRDFaHTML: function(r, mode) {
+      var s = '', about = '', property = '', rel = '', resource = '', href = '', content = '', langDatatype = '', typeOf = '', idValue = '', id = '';
 
-      if ('about' in r && r.about != '') {
-        about = ' about="' + r.about + '"';
-      }
-      else {
-        about = ' about="#' + idValue + '"';
-      }
       if ('rel' in r && r.rel != '') {
         rel = ' rel="' + r.rel + '"';
       }
-      if ('property' in r && r.property != '') {
-        property = ' property="' + r.property + '"';
-      }
-      else {
-        //TODO: Figure out how to use user's prefered vocabulary.
-        property = ' property="rdfs:label"';
-      }
-      if ('resource' in r && r.resource != '') {
-        resource = ' resource="' + r.resource + '"';
-      }
+
       if ('href' in r && r.href != '') {
         href = ' href="' + r.href + '"';
       }
-      if ('content' in r && r.content != '') {
-        content = ' content="' + r.content + '"';
-      }
-      if ('lang' in r && r.lang != '') {
-        langDatatype = ' xml:lang="' + r.lang + '" lang="' + r.lang + '"';
-      }
-      else {
-        if ('datatype' in r && r.datatype != '') {
-          langDatatype = ' datatype="' + r.datatype + '"';
+
+      if(mode == 'expanded') {
+        idValue = DO.U.generateAttributeId();
+        id = ' id="' + idValue + '"';
+
+        if ('about' in r && r.about != '') {
+          about = ' about="' + r.about + '"';
         }
-      }
-      if ('typeOf' in r && r.typeOf != '') {
-        typeOf = ' typeof="' + r.typeOf + '"';
+        else {
+          about = ' about="#' + idValue + '"';
+        }
+
+        if ('property' in r && r.property != '') {
+          property = ' property="' + r.property + '"';
+        }
+        else {
+          //TODO: Figure out how to use user's preferred vocabulary.
+          property = ' property="rdfs:label"';
+        }
+
+        if ('resource' in r && r.resource != '') {
+          resource = ' resource="' + r.resource + '"';
+        }
+
+        if ('content' in r && r.content != '') {
+          content = ' content="' + r.content + '"';
+        }
+
+        if ('lang' in r && r.lang != '') {
+          langDatatype = ' xml:lang="' + r.lang + '" lang="' + r.lang + '"';
+        }
+        else {
+          if ('datatype' in r && r.datatype != '') {
+            langDatatype = ' datatype="' + r.datatype + '"';
+          }
+        }
+
+        if ('typeOf' in r && r.typeOf != '') {
+          typeOf = ' typeof="' + r.typeOf + '"';
+        }
       }
 
       var element = (href == '') ? 'span' : 'a';
@@ -5615,7 +5625,7 @@ WHERE {\n\
                     textContent: this.base.selection
                     // lang: '' and/or xmllang: ''
                   };
-                  ref = DO.U.createRDFaHTML(noteData);
+                  ref = DO.U.createRDFaHTML(noteData, 'expanded');
                   break;
 
                 case 'bookmark':
