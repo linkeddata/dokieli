2d01cf949dc016822f828e3327504f93cc32d6b3
Sarven Capadisli <info@csarven.ca>
2017-11-14 16:03:15 +0100

    Fix URL for latest version for mutable resource

--- src/dokieli.js
+++ src/dokieli.js
@@ -1946,7 +1946,7 @@ console.log('createMutableResource ' + mutableURL);
 
       data = doc.getDocument();
       //TODO: Change to POST
-      DO.U.processPut(url, data, options);
+      DO.U.processPut(mutableURL, data, options);
 
 
 
