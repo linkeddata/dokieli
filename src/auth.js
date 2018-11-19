9cf730c03ec86fc27f89d8d95b47108268f2c58e
Sergey Malinin <sergmalinin@gmail.com>
2018-11-20 00:02:11 +1000

    Add using of solidAuth.trackSession() for check, if User is SignedIn already

--- src/auth.js
+++ src/auth.js
@@ -75,54 +75,77 @@ function showUserSigninSignout (node) {
   if (!userInfo) {
     var s = ''
 
-    if (Config.User.IRI) {
-      s = getUserSignedInHTML()
-    }
-    else {
-      s = '<button class="signin-user" title="Sign in to authenticate"><i class="fa fa-user-secret fa-2x"></i>Sign in</button>'
-    }
+    solidAuth.trackSession(session => {
+      var webId = session ? session.webId : null;
 
-    node.insertAdjacentHTML('beforeend', '<section id="user-info">' + s + '</section>')
-
-    userInfo = document.getElementById('user-info')
-
-    userInfo.addEventListener('click', function(e) {
-      e.preventDefault()
-      e.stopPropagation()
-
-      if (e.target.closest('.signout-user')) {
-        if (Config.User.OIDC && solidAuth) {
-          solidAuth.logout();
-        }
-
-        storage.removeStorageProfile()
-
-        Config.User = {
-          IRI: null,
-          Role: 'social',
-          UI: {}
-        }
-
-        util.removeChildren(node);
-
-        var documentMenu = document.querySelector('#document-menu')
-
-        showUserSigninSignout(documentMenu.querySelector('header'))
-
-        var ra = documentMenu.querySelector('.resource-activities');
-        ra.disabled = true;
-        ra.innerHTML = '<i class="fa fa-bolt fa-2x"></i>Activities';
+      if (webId && webId != Config.User.IRI) {
+        setUserInfo(webId, true)
+          .then(() => {
+            userInfo = document.getElementById('user-info');
+            if (!userInfo)
+              showUserSigninSignoutEnd(node);
+          })
+      } else {
+         userInfo = document.getElementById('user-info');
+         if (!userInfo)
+           showUserSigninSignoutEnd(node);
       }
-    });
-
-    var su = document.querySelector('#document-menu button.signin-user')
-    if (su) {
-      su.addEventListener('click', showUserIdentityInput)
-    }
-
-    var rA = document.querySelector('#document-menu .resource-activities')
-    if(rA) { rA.setAttribute('disabled', 'disabled') }
+    })
   }
+}
+
+
+function showUserSigninSignoutEnd (node) {
+  var s = ''
+
+  if (Config.User.IRI) {
+    s = getUserSignedInHTML()
+  }
+  else {
+    s = '<button class="signin-user" title="Sign in to authenticate"><i class="fa fa-user-secret fa-2x"></i>Sign in</button>'
+  }
+
+  node.insertAdjacentHTML('beforeend', '<section id="user-info">' + s + '</section>')
+
+  var userInfo = document.getElementById('user-info')
+
+  userInfo.addEventListener('click', function(e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    if (e.target.closest('.signout-user')) {
+      if (Config.User.OIDC) {
+        solidAuth.logout();
+      }
+
+      storage.removeStorageProfile()
+
+      Config.User = {
+        IRI: null,
+        Role: 'social',
+        UI: {}
+      }
+
+      util.removeChildren(node);
+
+      var documentMenu = document.querySelector('#document-menu')
+
+      showUserSigninSignout(documentMenu.querySelector('header'))
+
+      var ra = documentMenu.querySelector('.resource-activities');
+      ra.disabled = true;
+      ra.innerHTML = '<i class="fa fa-bolt fa-2x"></i>Activities';
+    }
+  });
+
+  var su = document.querySelector('#document-menu button.signin-user')
+  if (su) {
+    su.addEventListener('click', showUserIdentityInput)
+  }
+
+  var rA = document.querySelector('#document-menu .resource-activities')
+  if(rA) { rA.setAttribute('disabled', 'disabled') }
+
 }
 
 
