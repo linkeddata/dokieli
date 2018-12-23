a53c87f8bae2a6faf4788708299142e7210b447b
Sarven Capadisli <info@csarven.ca>
2018-12-23 12:01:14 +0100

    Show action message when opening a resource

--- src/dokieli.js
+++ src/dokieli.js
@@ -840,6 +840,8 @@ var DO = {
 
       var open = DO.U.urlParam('open');
       if (open) {
+        DO.U.showActionMessage(document.documentElement, '<span class="progress"><svg class="fas fa-circle-notch fa-spin fa-fw" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"/></svg> Opening <a href="' + open + '" target="_blank">' + open + '</a></span>', {'timer': 10000});
+
         DO.U.openResource(open);
 
         window.history.replaceState({}, null, document.location.href.substr(0, document.location.href.lastIndexOf('?')));
@@ -2617,13 +2619,16 @@ var DO = {
       })
     },
 
-    showActionMessage: function(node, message) {
+    showActionMessage: function(node, message, options) {
+      options = options || {};
+      options['timer'] = ('timer' in options) ? options.timer : 1500;
+
       var message = '<aside id="document-action-message" class="do on"><p>' + message + '</p></aside>';
       node.appendChild(util.fragmentFromString(message));
       window.setTimeout(function () {
         var dam = document.getElementById('document-action-message');
         dam.parentNode.removeChild(dam);
-      }, 1500);
+      }, options.timer);
     },
 
     shareResource: function shareResource (e, iri) {
