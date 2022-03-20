8ccc405c01f757c0a31995b1a9ebea709f6ad3fb
Virginia Balseiro <virginia.balseiro@inrupt.com>
2022-03-20 12:38:37 +0100

    Chore: update dependencies and fix vulnerabilities
    - add uglifyjs, fix minify script
    
    - override xmldom with secure version
    
    - override ansi-regex with secure version
    
    - remove webpack-core
    
    - upgrade standard
    
    - upgrade node-fetch and override with secure version
    
    - add buffer and process
    
    - patch wrapper-webpack-plugin
    
    - upgrade webpack to v5, update config
    
    - upgrade npm
    
    - fix protocol in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,69 @@
-const path = require('path')
-const fs = require('fs')
-const WrapperPlugin = require('wrapper-webpack-plugin')
-const headerDoc = fs.readFileSync(require.resolve('solid-auth-client/dist-lib/solid-auth-client.bundle.js'), 'utf8') + '\n';
+const webpack = require('webpack')
+const path = require("path");
+const fs = require("fs");
+const WrapperPlugin = require("wrapper-webpack-plugin");
+const headerDoc =
+  fs.readFileSync(
+    require.resolve("solid-auth-client/dist-lib/solid-auth-client.bundle.js"),
+    "utf8"
+  ) + "\n";
 
 module.exports = {
+  resolve: {
+    modules: [path.join(__dirname, "node_modules")],
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      url: false,
+      "https-browserify": false,
+      stream: false,
+      "stream-browserify": false,
+      crypto: false,
+      buffer: require.resolve("buffer/"),
+    },
+    extensions: [".ts", ".js", ".mjs"],
+  },
   mode: "none",
-  entry: [
-    './src/dokieli.js'
-  ],
+  entry: ["./src/dokieli.js"],
   output: {
-    path: path.join(__dirname, '/scripts/'),
-    filename: 'dokieli.js',
-    library: 'DO',
-    libraryTarget: 'window'
+    path: path.join(__dirname, "/scripts/"),
+    filename: "dokieli.js",
+    library: "DO",
+    libraryTarget: "window",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         // loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   externals: {
-    'node-fetch': 'fetch',
-    'text-encoding': 'TextEncoder',
-    'whatwg-url': 'window',
-    'isomorphic-fetch': 'fetch',
-    '@trust/webcrypto': 'crypto',
-    'solid-auth-client': ['solid', 'auth']
+    "node-fetch": "fetch",
+    "text-encoding": "TextEncoder",
+    "whatwg-url": "window",
+    "isomorphic-fetch": "fetch",
+    "@trust/webcrypto": "crypto",
+    "solid-auth-client": ["solid", "auth"],
   },
-  devtool: 'source-map',
+  devtool: "source-map",
 
   plugins: [
     new WrapperPlugin({
-      header: headerDoc
-    })
-  ]
-}
+      header: headerDoc,
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+  }),
+  ],
+};
