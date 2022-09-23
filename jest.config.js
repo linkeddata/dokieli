f4acbbca959000ef53a8fce9761462dece359e11
Virginia Balseiro <virginiabalseiro@gmail.com>
2022-09-23 12:03:46 +0200

    move converage to tests dir

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
+  coverageDirectory: "tests/coverage",
   coverageProvider: "v8",
   testEnvironment: "jsdom",
 };
