cb358478b29f13ef75d76d7f7dbdd529df952732
Sarven Capadisli <info@csarven.ca>
2022-10-16 19:34:02 +0200

    Add TestDescriptionReviewStatus to Vocab

--- src/config.js
+++ src/config.js
@@ -280,6 +280,15 @@ module.exports = {
     'http://purl.org/spar/cito/usesMethodIn': 'uses method in'
   },
 
+  TestDescriptionReviewStatus: {
+    'http://www.w3.org/2006/03/test-description#accepted': "the item has gone through a first review, which shows it as valid for further processing",
+    'http://www.w3.org/2006/03/test-description#approved': "the item has gone through the review process and was approved",
+    'http://www.w3.org/2006/03/test-description#assigned': "a more specific review of the item has been assigned to someone",
+    'http://www.w3.org/2006/03/test-description#onhold': "the item had already gone through the review process, but the results of the review need to be re-assessed due to new input",
+    'http://www.w3.org/2006/03/test-description#rejected': "the item has gone through the review process and was rejected",
+    'http://www.w3.org/2006/03/test-description#unreviewed': "the item has been proposed, but hasn't been reviewed (e.g. for completeness) yet"
+  },
+
   AvailableMediaTypes: ['text/turtle', 'application/ld+json', 'application/rdf+xml', 'application/xhtml+xml', 'text/html'],
 
   AcceptBinaryTypes: ['image/png', 'image/jpeg', 'image/gif'],
