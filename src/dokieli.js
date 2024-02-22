1f88d6f562466dd1326e5db993d05cef26f75de0
Sarven Capadisli <info@csarven.ca>
2024-02-22 22:13:26 +0100

    Add xml:link to SVG

--- src/dokieli.js
+++ src/dokieli.js
@@ -613,6 +613,7 @@ var DO = {
         .attr('about', '#' + id)
         .attr('class', 'graph')
         .attr('xmlns', 'http://www.w3.org/2000/svg')
+        .attr('xml:xlink', 'http://www.w3.org/1999/xlink')
         .attr('xml:lang', options.language)
         .attr('prefix', 'rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns# rdfs: http://www.w3.org/2000/01/rdf-schema# xsd: http://www.w3.org/2001/XMLSchema# dcterms: http://purl.org/dc/terms/');
 
