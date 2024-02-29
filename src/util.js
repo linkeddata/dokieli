9dcf637e5bb25f8703d221c61b92a99a838068dd
Virginia Balseiro <virginiabalseiro@gmail.com>
2024-02-29 23:39:04 +0100

    refactor uniqueArray function in util

--- src/util.js
+++ src/util.js
@@ -22,15 +22,7 @@ module.exports = {
  * @returns {Array}
  */
 function uniqueArray(a) {
-  var n = {};
-  var r = [];
-  for (var i = 0; i < a.length; i++) {
-    if (!n[a[i]]) {
-      n[a[i]] = true;
-      r.push(a[i]);
-    }
-  }
-  return r;
+  return Array.from(new Set(a));
 }
 
 function getDateTimeISO() {
