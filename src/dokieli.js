3650e7d5529270b88b390bba2f0d91c46bcd6263
Sarven Capadisli <info@csarven.ca>
2024-03-12 00:57:27 +0100

    Move remaining SVG icons to template

--- src/dokieli.js
+++ src/dokieli.js
@@ -6460,7 +6460,7 @@ console.log('//TODO: Handle server returning wrong Response/Content-Type for the
                 var responseMessage = e.target.parentNode;
                 responseMessage.insertAdjacentHTML('beforeend',
                   '<span class="progress" data-to="' + inboxURL +
-                  '"><svg class="fas fa-circle-notch fa-spin fa-fw" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"/></svg></span>')
+                  '">' + Icon[".fas.fa-circle-notch.fa-spin.fa-fw"] + '</span>')
 
                 var notificationStatements = `<dl about="` + objectId + `" prefix="acl: http://www.w3.org/ns/auth/acl#">
   <dt>Object type</dt><dd><a about="` + objectId + `" href="` + DO.C.Vocab['aclAuthorization']['@id'] + `" typeof="acl:Authorization">Authorization</a></dd>
@@ -6485,14 +6485,14 @@ console.log('//TODO: Handle server returning wrong Response/Content-Type for the
 
                     responseMessage
                       .querySelector('.progress[data-to="' + inboxURL + '"]')
-                      .innerHTML = '<svg class="fas fa-times-circle fa-fw" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"/></svg> Unable to notify. Try later.'
+                      .innerHTML = Icon[".fas.fa-times-circle.fa-fw"] + ' Unable to notify. Try later.'
                   })
                   .then(response => {
                     var notificationSent = 'Notification sent';
                     var location = response.headers.get('Location');
 
                     if (location) {
-                      notificationSent = '<a target="_blank" href="' + location.trim() + '"><svg class="fas fa-check-circle fa-fw" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"/></svg></a>'
+                      notificationSent = '<a target="_blank" href="' + location.trim() + '">' + Icon[".fas.fa-check-circle.fa-fw"] + '</a>'
                     }
                     else {
                       notificationSent = notificationSent + ", but location unknown."
