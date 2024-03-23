dd03437fdb965da6a508b3e4f4e8965c8ed97341
Sarven Capadisli <info@csarven.ca>
2024-03-23 13:37:22 +0100

    Add calculateDistance to geo

--- src/geo.js
+++ src/geo.js
@@ -1,4 +1,15 @@
 
+//From https://stackoverflow.com/a/21623206
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+          c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
 function roundValue(value, decimals) {
   return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
