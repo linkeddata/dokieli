1c1da49829ccf85a463cc4075ead0387c7cbe313
Sarven Capadisli <info@csarven.ca>
2024-06-09 19:25:48 +0200

    Add containsSPARQLVariable

--- src/util.js
+++ src/util.js
@@ -165,6 +165,20 @@ function isValidISBN (str) {
   const pattern = new RegExp(regex);
   return pattern.test(str);
 }
+//https://www.w3.org/TR/sparql11-query/#rVar
+function containsSPARQLVariable(str) {
+  const pnCharsBase = "A-Za-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD";
+  const pnCharsU = `${pnCharsBase}_`;
+  const varName = `(?:[${pnCharsU}]|[0-9])(?:[${pnCharsU}]|[0-9]|\\u00B7|[\\u0300-\\u036F]|[\\u203F-\\u2040])*`;
+
+  const var1Pattern = `\\?[${pnCharsU}](${varName})`;
+  const var2Pattern = `\\$[${pnCharsU}](${varName})`;
+
+  const varRegex = new RegExp(`(${var1Pattern}|${var2Pattern})`, 'u');
+
+  return varRegex.test(str);
+}
+
 
 export {
   uniqueArray,
@@ -182,5 +196,6 @@ export {
   hashCode,
   sortToLower,
   matchAllIndex,
-  isValidISBN
+  isValidISBN,
+  containsSPARQLVariable
 };
